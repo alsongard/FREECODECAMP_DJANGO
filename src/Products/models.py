52338f8530985a4588902bbafa4b88035f16f455
alsongard <alsongard@gmail.com>
2024-11-14 09:42:05 -0500

    added soem new fields for the class Products and blank=False

--- src/Products/models.py
+++ src/Products/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    title       = models.TextField()
-    description = models.TextField()
-    price       = models.TextField()
+    productNumber = models.AutoField(primary_key=True)
+    category    = models.CharField(max_length=100, blank=False)
+    title       = models.CharField(max_length=80, blank=False)
+    description = models.TextField(blank=False)
+    price       = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
     summary     = models.TextField(default='All Products are Amazing')
+    available   = models.BooleanField(default=True)
