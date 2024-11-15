fec44e1758dd8f2197c4079ee0257304d4f17c0a
alsongard <alsongard@gmail.com>
2024-11-15 07:24:38 -0500

    request parameter

--- src/Pages/views.py
+++ src/Pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import *
+
 # Create your views here.
 def contact_view(request, *args, **kwargs):
     print(f"*args : {args} \n **kwargs: {kwargs}")
     print(request)
     print(request.user)
-    return HttpResponse("<h1>This is my contact Page</h1>")
+    # return HttpResponse("<h1>This is my contact Page</h1>")
+    return render(request, "contact.html", {})
 
 
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>This is my about view</h1>")
+def about_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>This is my about view</h1>")
+    return render(request, "about.html", {})
