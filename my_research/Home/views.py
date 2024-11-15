2864f0c693f2eb193a0b467ea135d6ebaa67ad37
alsongard <alsongard@gmail.com>
2024-11-15 08:36:01 -0500

    updated views for rendering html template files

--- my_research/Home/views.py
+++ my_research/Home/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Welcome to Zetech University</h1>")
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Welcome to Zetech University</h1>")
+    return render(request, "home.html", {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
