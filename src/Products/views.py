2a62c31019d0a7c8b9bee08b7a60d1b3589a503a
alsongard <alsongard@gmail.com>
2024-11-15 16:59:12 -0500

    added product_database_view

--- src/Products/views.py
+++ src/Products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Products
+
+def products_database_view(request):
+    obj = Products.objects.get(productNumber=1)
+    # context = {
+    #     'category': obj.category,
+    #     'title' : obj.title,
+    #     'description': obj.description,
+    #     'price': obj.price
+    # }
+    context = {
+        'object': obj
+    }
+    return render (request, 'products/detail.html', context)
