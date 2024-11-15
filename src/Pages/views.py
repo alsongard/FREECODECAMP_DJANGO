a5e2b6b5b95e744e45cc977e52df581a230dfcb0
alsongard <alsongard@gmail.com>
2024-11-15 05:37:55 -0500

    added contact and about views to views.py

--- src/Pages/views.py
+++ src/Pages/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import *
 # Create your views here.
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
+    print(f"*args : {args} \n **kwargs: {kwargs}")
+    print(request)
+    print(request.user)
     return HttpResponse("<h1>This is my contact Page</h1>")
 
 
 def about_view(*args, **kwargs):
     return HttpResponse("<h1>This is my about view</h1>")
-
