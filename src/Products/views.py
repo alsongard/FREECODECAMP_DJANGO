6aefc7c367b275eac57393fcab584aa3ae0af14b
alsongard <alsongard@gmail.com>
2024-11-16 06:05:06 -0500

    create product_create_view for form submission

--- src/Products/views.py
+++ src/Products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Products
+from .forms import ProductForms
 
 def products_database_view(request):
     obj = Products.objects.get(productNumber=1)
@@ -16,3 +17,13 @@ def products_database_view(request):
         'object': obj
     }
     return render (request, 'products/detail.html', context)
+
+def product_create_view(request):
+    form = ProductForms(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForms()
+    context = {
+        'form' : form
+    }
+    return render (request, product_create_view, context)
