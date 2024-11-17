aa9d595456555570188043f0e0691046bb40df14
alsongard <alsongard@gmail.com>
2024-11-17 19:20:27 +0300

    null and blank model field attributes

--- src/Products/models.py
+++ src/Products/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     productNumber = models.AutoField(primary_key=True)
-    category    = models.CharField(max_length=100, blank=False)
-    title       = models.CharField(max_length=80, blank=False)
+    category    = models.CharField(max_length=100,null=False, blank=False)
+    title       = models.CharField(max_length=80, null=False, blank=False)
     description = models.TextField(blank=False)
     price       = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
     summary     = models.TextField(default='All Products are Amazing')
